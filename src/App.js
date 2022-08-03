94a7ef6016ee36335558f5a461be6fb425bc1b0b
Chetan Chouhan <mousegamingv2.0@gmail.com>
2022-08-03 18:26:26 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Header from './Container/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductListing from './Container/ProductListing';
+import ProductDetails from './Container/ProductDetails';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Hello From App Component</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route to='/' element={ProductListing} />
+          <Route to='/product/:productId' element={ProductDetails} />
+          <Route> 404 Page Not Found!</Route>
+        </Routes>
+      </Router>
+      {/* <h1>Hello From App Component</h1> */}
     </div>
   );
 };
